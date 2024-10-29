b244f8b63dda94f216a36f1775c7d2a50aadc64d
Julian Enrique <julian.buitrago3003@gmail.com>
2024-10-29 09:10:28 -0500

    initial commit

---
+++ src/main/java/co/edu/usco/backendparcial/BackendParcialApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usco.backendparcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendParcialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendParcialApplication.class, args);
+    }
+
+}
